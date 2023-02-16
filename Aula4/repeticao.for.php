fe0fd9842f6efe84eae510803f3dfc9087b48021
Rafael Tiago Franco <15355979688@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-02-16 11:32:58 -0300

    Laço de repetição for

---
+++ Aula4/repeticao.for.php
@@ -0,0 +1,24 @@
+<h2>Estrutura de repetição for</h2>
+
+<p>
+    Utilizada para repetir uma sequência de
+    instrções de um número predeterminado de vezes.
+</p>
+
+<p>
+    <pre>
+        for(inicio; condição; passo)
+        {
+            //instruções a serem repetidas
+        }
+    </pre>
+
+</p>
+
+    <?php
+        for($contador =0; $contador <10;$contado++)
+        {
+            echo "2 x $contador = " . $contador * 2. " <br> ";
+        }
+
+?>
