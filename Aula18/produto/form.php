2234cd7c1b8e03b02d1181dc507d5ea51b355c56
Rafael Tiago Franco <15355979688@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-05-18 10:34:44 -0300

    uploads

--- Aula18/produto/form.php
+++ Aula18/produto/form.php
@@ -1,7 +1,7 @@
-<?php require_once "consultar_por_id.php"?>
+<?php require_once"consultar_por_id.php"?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,17 +11,20 @@
 <body>
     <h1>Cadratro de Produto</h1>
     <hr>
-    <form action="<?php echo isset($produto) ? 'atualizar.php' : "inserir.php" ?>" method="post">
+    <form action="<?php  echo isset($produto) ? 'atualizar.php' : 'inserir.php' ?>" 
+    method="post" 
+    
+    enctype="multipart/form-data">
 
     <label for="name">Nome</label><br>
     <input type="text" name="nome" id="nome" value="<?php echo $produto['nome'] ?? ""; ?>"><br>
     <input type="hidden" name="id" id="id" value="<?php echo $produto['idproduto'] ?? ""; ?>"><br>
 
     <label for="descricao">Descrição</label><br>
-    <textarea name= "descricao" id="descricao"><?php echo $produto['descricao'] ?? "" ; ?></textarea><br>
+    <textarea name= "descricao" id="descricao"><?php echo $produto['descricao'] ?? ""; ?></textarea><br>
 
     <label for="preco">Preço</label><br>
-    <input type="number"  step="0.01" name="preco" id="preco" value="<?php echo $produto['preco'] ?? "0.00" ; ?>><br>
+    <input type="number"  step="0.01" name="preco" id="preco" value="<?php echo $produto['preco'] ?? "0.00"; ?>"><br>
 
     <label for="foto">Foto</label><br>  
     <input type="file" name="foto" id="foto"><br><br>
