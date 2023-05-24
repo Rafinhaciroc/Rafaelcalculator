e6c999faba9f273be3d821cc73c430f58ec8fe27
Rafael Tiago Franco <15355979688@dc01.ifs.ifsuldeminas.edu.br>
2023-05-24 12:26:30 -0300

    Rafinha

---
+++ Aula18/usuario/consultar_todos.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once"../conexao.php";
+
+
+//String com o comando SQL oara ser executado do DB
+$sql = "SELECT*FROM `usuario` ";
+        
+//Prepara o SQL par ser executado no banco de dados
+ $comando = $conexao->prepare($sql);
+
+//executa o SQL - Comando no Banco de Dados
+$comando->execute();
+
+//pegar o resultado da consulta
+$resultado = $comando ->get_result();
+
+//criar um vetor vazio
+$usuarios = [];
+
+
+//pegar a todas as linha do resultado
+while($usuario = $resultado->fetch_assoc()){
+        //adiciona o produto (linha do resultado) no vetor
+        $usuarios[] = $usuario;
+
+
+
+}
+
+
+
+?>
+
+
+
+
+
+
+
