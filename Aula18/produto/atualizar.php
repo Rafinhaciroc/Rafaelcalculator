c230e3500b785b0b0406b60f5935e14d8e6f0623
Rafael Tiago Franco <15355979688@dc01.ifs.ifsuldeminas.edu.br>
2023-06-15 09:31:07 -0300

    Trabalho

--- Aula18/produto/atualizar.php
+++ Aula18/produto/atualizar.php
@@ -1,7 +1,7 @@
 <?php require_once "../controla_sessao/controla.php";?>
 <?php
 
-require_once"../conexao.php";
+require_once "../conexao.php";
 
 if(isset($_POST["nome"]) &&
  isset($_POST["descricao"]) && 
